0dc3a2e84d2426620878c1114cf986753d4e568c
harishsivabalan <harishsiva8885@gmail.com>
2025-06-11 15:53:35 +0530

    Add files via upload

---
+++ server/Controller/AdminController.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const getCredentials = (req, res) => {
+    const Credentials = { "username": "admin", "password": "1234" }
+    res.status(200).json(Credentials)
+}
+
+module.exports = { getCredentials }
+
+
+
